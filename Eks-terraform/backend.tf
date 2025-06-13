5def6d33cc322767dacf64cccf17dff0dd972d0b
Umangptel4681 <umangpatel4681@gmail.com>
2025-06-13 15:29:59 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "umang902345" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ca-central-1"
   }
 }
