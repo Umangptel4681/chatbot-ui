b5a622336e1b5484a2f874f40cb6e5d3022b1233
Umangptel4681 <umangpatel4681@gmail.com>
2025-06-13 15:41:41 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "eks_igw" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ca-central-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "ca-central-1b"
   map_public_ip_on_launch = true
 
   tags = {
